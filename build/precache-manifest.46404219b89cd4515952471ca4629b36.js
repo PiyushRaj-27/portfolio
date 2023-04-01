97e940d2c9e7c81f564900cac1de944c14f8ae9f
Piyush Raj <99380058+PiyushRaj-27@users.noreply.github.com>
2023-04-01 20:05:40 +0530

    Adding the Final build files here!

---
+++ build/precache-manifest.46404219b89cd4515952471ca4629b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9084e317bf26e6c11276",
+    "url": "/static/css/main.f5bef374.chunk.css"
+  },
+  {
+    "revision": "9084e317bf26e6c11276",
+    "url": "/static/js/main.9084e317.chunk.js"
+  },
+  {
+    "revision": "8cf608ac7dd636455947",
+    "url": "/static/js/1.8cf608ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac74c7b50daa1603905fc2a638d1c413",
+    "url": "/static/media/bg2.ac74c7b5.jpg"
+  },
+  {
+    "revision": "abc129c7ba8d69665dd28e9b0876a119",
+    "url": "/index.html"
+  }
+];
