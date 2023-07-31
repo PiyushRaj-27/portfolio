c9f7ff8553a91613ce807ed1a2a434458144ecc4
Piyush Raj <99380058+PiyushRaj-27@users.noreply.github.com>
2023-07-31 20:01:36 +0530

    Update new portfolio

---
+++ precache-manifest.8dd4f6117e5fa614f66627daf48f2822.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2df71a7d043feed54d1f",
+    "url": "/static/css/main.7d262a70.chunk.css"
+  },
+  {
+    "revision": "2df71a7d043feed54d1f",
+    "url": "/static/js/main.2df71a7d.chunk.js"
+  },
+  {
+    "revision": "8cf608ac7dd636455947",
+    "url": "/static/js/1.8cf608ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac74c7b50daa1603905fc2a638d1c413",
+    "url": "/static/media/bg2.ac74c7b5.jpg"
+  },
+  {
+    "revision": "8dfd5cf167c9187d0a69256070b05cc4",
+    "url": "/index.html"
+  }
+];
